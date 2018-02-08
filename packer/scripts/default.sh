c3599b2e00184417c40dfa1ab4a80bf6f9cd89d2
Ubuntu <ubuntu@ip-172-31-6-252.us-west-2.compute.internal>
2018-02-08 19:23:10 +0000

    Build a logastash 6.x AMI

---
+++ packer/scripts/default.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+# Updating and Upgrading dependencies
+apt-get update
---
+++ packer/scripts/install-logstash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install logstash
+
