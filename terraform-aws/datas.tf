47e37394302498b42f247ab408720033c630472e
Moe Sy <moe@wslogistics.io>
2018-02-08 20:44:07 +0000

    + Userdata script

---
+++ terraform-aws/datas.tf
@@ -0,0 +1,13 @@
+data "template_file" "client_userdata_script" {
+  template = "${file("${path.module}/../templates/user_data.sh")}"
+
+  vars {
+    cloud_provider          = "aws"
+    volume_name             = ""
+    logstash_cluster        = "${var.logstash_cluster}"
+    logstash_environment    = "${var.environment}-${var.logstash_cluster}"
+    security_groups         = "${aws_security_group.logstash_security_group.id}"
+    aws_region              = "${var.aws_region}"
+    availability_zones      = "${var.availability_zones}"
+  }
+}
