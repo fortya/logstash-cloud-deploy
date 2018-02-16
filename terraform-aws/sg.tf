cd157fab68336ef618a8068ed47c2a28240ae30f
Pipe Martina <felimartina@outlook.com>
2018-02-15 19:16:22 -1000

    Added subnets to enhance security. Still need to find a better way to grab subnets for SGs though

--- terraform-aws/sg.tf
+++ terraform-aws/sg.tf
@@ -13,22 +13,25 @@ resource "aws_security_group" "logstash_security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["${var.admin_cidrs}"]
+    description = "Allow SSH access to admin users"
+  }
+
+  #TODO: We should only allow access from public subnets
+  ingress {
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "CHANGE!!! - Allow logstash traffic from everywhere"
   }
 
   ingress {
-    from_port = 5044
-    to_port   = 5044
-    protocol  = "tcp"
-    self      = true
-  }
-
-  # allow inter-cluster ping
-  ingress {
-    from_port = 8
-    to_port   = 0
-    protocol  = "icmp"
-    self      = true
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    self        = true
+    description = "Allow inter-cluster ping"
   }
 
   egress {
@@ -36,6 +39,7 @@ resource "aws_security_group" "logstash_security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbounb traffic"
   }
 }
 
@@ -49,12 +53,12 @@ resource "aws_security_group" "logstash_elb_security_group" {
     cluster = "${var.logstash_cluster}"
   }
 
-  # allow HTTP access to client nodes via port 8080 - better to disable, and either way always password protect!
   ingress {
     from_port   = 5044
     to_port     = 5044
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow 5044 access from the internet"
   }
 
   egress {
@@ -62,5 +66,6 @@ resource "aws_security_group" "logstash_elb_security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbounb traffic"
   }
 }
